0ef212e791c65211c30fbb38d1a7c6b1e6ae2961
Hadiroudbari <hadiroudbari@gmail.com>
2023-10-01 13:58:32 +0330

    Finish Onboarding UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}', './node_modules/flowbite/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // 'dark-gary-bg': '#374151',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
