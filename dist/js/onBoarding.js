c229e19eb39338e5c46c24efa4dc1cb06f643fba
Hadiroudbari <hadiroudbari@gmail.com>
2023-10-05 10:42:56 +0330

    Change Onboarding with SwiperJS and Hide Scrollbar

--- dist/js/onBoarding.js
+++ dist/js/onBoarding.js
@@ -1,28 +1,22 @@
+const lastSlide = document.querySelector('#last__slide');
 const nextBtn = document.querySelector('#btn__next');
-const onboardingFirst = document.querySelector('#onboarding__first');
-const onboardingSecond = document.querySelector('#onboarding__second');
-const onboardingThird = document.querySelector('#onboarding__third');
-const indicatorFirst = document.querySelector('#indicator__first');
-const indicatorSecond = document.querySelector('#indicator__second');
-const indicatorThird = document.querySelector('#indicator__third');
-
-let index = 0;
-nextBtn.addEventListener('click', e => {
-  if (index === 0) {
-    onboardingFirst.style.transform = 'translateX(-100%)';
-    onboardingSecond.style.transform = `translateX(0)`;
-    onboardingThird.style.transform = 'translateX(100%)';
-    indicatorFirst.classList.remove('bg-gray-800');
-    indicatorFirst.classList.add('bg-gray-400');
-    indicatorSecond.classList.add('bg-gray-800');
+const nextStart = document.querySelector('#btn__start');
+const swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+nextBtn.addEventListener('click', e => swiper.slideNext());
+swiper.on('slideChange', function () {
+  nextBtn.classList.add('table');
+  nextBtn.classList.remove('hidden');
+  nextStart.classList.add('hidden');
+  nextStart.classList.remove('table');
+  if (lastSlide.classList.contains('swiper-slide-next')) {
+    nextBtn.classList.remove('table');
+    nextBtn.classList.add('hidden');
+    nextStart.classList.remove('hidden');
+    nextStart.classList.add('table');
   }
-  if (index === 1) {
-    onboardingFirst.style.transform = 'translateX(-200%)';
-    onboardingSecond.style.transform = `translateX(-100%)`;
-    onboardingThird.style.transform = 'translateX(0)';
-    indicatorSecond.classList.remove('bg-gray-800');
-    indicatorThird.classList.add('bg-gray-800');
-    nextBtn.textContent = 'Get Started';
-  }
-  index++;
 });
